7b342e631f49e278c4c160fbec3c96134ebcce8e
Hawaa <hawaazaroual@gmail.com>
2021-08-03 14:37:28 -0600

    making the delete button, and adding the delete post request

--- expressServer.js
+++ expressServer.js
@@ -2,25 +2,34 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
-
-function generateRandomString() {
-
-}
-
 const bodyParser = require("body-parser");
+const { request, response } = require("express");
 app.use(express.urlencoded({extended: true}));
-
-app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
-});
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString() {
+  return Math.random().toString(36).substr(2, 6);
+};
+
+
+//this returns a message in our terminal when the port is listening
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
+//this provides html template
 app.get("/urls/new", (req, res) => {
   res.render("urlsNew");
+});
+
+//this is the route to show user their new link - longURL
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urlsShow", templateVars);
 });
 
 app.get("/urls", (req, res) => {
@@ -28,26 +37,33 @@ app.get("/urls", (req, res) => {
   res.render("urlsIndex", templateVars);
 });
 
+//requests to this endpoint will redirect to longURL
+app.get("/u/:shortURL", (req, res) => {
+  res.redirect(urlDatabase[req.params['shortURL']]);
+});
 
-app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase };
-  res.render("urls_show", templateVars);
+app.post("/urls", (req, res) => {
+  const shortURL = generateRandomString();
+  if (!(req.body.longURL).includes('http')) {
+    req.body.longURL = 'http://' + req.body.longURL;
+  }
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
+});
+
+app.post("/urls", (req, res) => {
+delete urlDatabase[req.params.shortURL];
+  res.redirect('/urls');
 });
 
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  res.send("Hello from TinyApp");
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-
-
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
