977ddf12d8fc65690fc237f14089da87e82a1cf5
Hawaa <hawaazaroual@gmail.com>
2021-08-02 18:27:42 -0600

    URL shortener part 1 completed

--- expressServer.js
+++ expressServer.js
@@ -3,15 +3,31 @@ const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 
+function generateRandomString() {
+
+}
+
+const bodyParser = require("body-parser");
+app.use(express.urlencoded({extended: true}));
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+app.get("/urls/new", (req, res) => {
+  res.render("urlsNew");
+});
 
 app.get("/urls", (req, res) => {
   const templateVars = {urls:urlDatabase};
   res.render("urlsIndex", templateVars);
 });
+
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase };
