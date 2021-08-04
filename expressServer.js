988fa34bdd8140d1111f0483f7ba569e60a38e9e
Hawaa <hawaazaroual@gmail.com>
2021-08-03 22:36:34 -0600

    working on login and username. Cookies are tracked

--- expressServer.js
+++ expressServer.js
@@ -4,17 +4,16 @@ const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 const bodyParser = require("body-parser");
 const { request, response } = require("express");
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b2xVn2: "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
 };
 
 function generateRandomString() {
   return Math.random().toString(36).substr(2, 6);
-};
-
+}
 
 //this returns a message in our terminal when the port is listening
 app.listen(PORT, () => {
@@ -28,38 +27,53 @@ app.get("/urls/new", (req, res) => {
 
 //this is the route to show user their new link - longURL
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+  };
   res.render("urlsShow", templateVars);
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = {urls:urlDatabase};
+  const templateVars = { urls: urlDatabase };
   res.render("urlsIndex", templateVars);
 });
 
 //requests to this endpoint will redirect to longURL
 app.get("/u/:shortURL", (req, res) => {
-  res.redirect(urlDatabase[req.params['shortURL']]);
+  res.redirect(urlDatabase[req.params["shortURL"]]);
 });
 
 app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
-  if (!(req.body.longURL).includes('http')) {
-    req.body.longURL = 'http://' + req.body.longURL;
+  if (!req.body.longURL.includes("http")) {
+    req.body.longURL = "http://" + req.body.longURL;
   }
   urlDatabase[shortURL] = req.body.longURL;
   res.redirect(`/urls/${shortURL}`);
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {
-delete urlDatabase[req.params.shortURL];
-  res.redirect('/urls');
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls");
 });
 
 app.post("/urls/:shortURL", (req, res) => {
- urlDatabase[req.params['shortURL']] = req.body.Update;
- res.redirect('/urls'); 
+  urlDatabase[req.params["shortURL"]] = req.body.Update;
+  res.redirect("/urls");
 });
+
+//post login
+app.post("/login", (req, res) => {
+  res.cookie("Username", req.body.Username);
+  res.redirect("urls");
+});
+//display username
+const templateVars = {
+  username: req.cookies["username"],
+  urls: Databaase
+};
+res.render("urlsIndex", templateVars);
 
 app.get("/", (req, res) => {
   res.send("Hello from TinyApp");
